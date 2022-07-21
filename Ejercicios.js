44b69279c09dc1a8e4ed25b5f18e9aced93ae228
joaquinquiroga22 <joaquinquiroga22@gmail>
2022-07-21 18:06:27 -0300

    first commit

---
+++ Ejercicios.js
@@ -0,0 +1,45 @@
+function Obtener(obj) {
+    //Obtener y Recorrer las Llaves de un Objeto
+    //Aqui tu codigo
+
+}
+
+function Fecha(num) {
+    //Escriba una función de JavaScript para obtener el día de una fecha en particular.
+    //Aqui tu codigo
+
+}
+
+function verificarPassword(usuario, password) {
+    // Comprueba si la "password" enviada coincide con la propiedad "password" del objeto "usuario"
+    // Devuelve "true" si coinciden
+    // De lo contrario, devuelve "false"
+    // // Tu código:
+
+}
+
+function crearGato(nombre, edad) {
+    // Crear un nuevo objeto con la propiedad "nombre" y el valor definido como el argumento "nombre".
+    // Agrega una propiedad al objeto con el nombre "edad" y usa el valor definido en el argumento "edad"
+    // Agrega un método (funcion) llamado "meow" que devuelva el string "Meow!"
+    // Devuelve el objeto
+    // Tu código:
+
+}
+function agregarMetodoCalculoDescuento(producto) {
+    // Agregar un método (función) al objeto "producto" llamado "calcularPrecioDescuento"
+    // Este método debe multiplicar el "precio" del "producto" ("producto.precio" o "producto[precio]") y "porcentajeDeDescuento" para obtener el descuento
+    // El método resta el descuento del precio y devuelve el precio con descuento
+    // Devuelve el objeto "producto" al final de la función
+    // Ejemplo:
+    // producto.precio -> 20
+    // producto.porcentajeDeDescuento -> 0.2 (o simplemente ".2")
+    // producto.calcularPrecioDescuento() -> 20 - (20 * 0.2)
+    // Tu código:
+
+}
+function ordenar(array) {
+    // A partir del array pasado por parametros, ordenarlo (ascendente o descendente) de la forma que tu quieras
+    // Mostrar array ordenado.
+    // Tu código:
+}
